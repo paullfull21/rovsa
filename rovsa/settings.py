aea4a0f93270ca8ce19668b2795368f1a8bae8c1
paullfull21 <paul_spinoza@hotmail.com>
2015-03-04 15:27:22 -0700

    no se requiere

--- rovsa/settings.py
+++ rovsa/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-#heroku data base (Base de datos)
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
